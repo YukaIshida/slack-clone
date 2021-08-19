3df535e7b22da1f1b9ad356841d45afde0641e16
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-19 17:51:31 +0900

    チャンネル作成モーダルを別コンポーネント化

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -13,35 +13,7 @@
         <div class="mt-5 flex justify-between items-center">
             <div class="font-bold opacity-50 text-lg">チャンネル</div>
             <!-- modal -->
-            <div class="z-10 fixed top-0 left-0 h-full w-full flex items-center justify-center" 
-                style="background-color:rgba(0, 0, 0, 0.5)"
-                v-show="channelModal"
-                @click="closeChannelModal"
-            >
-                <div class="z-20 bg-white text-gray-900 w-1/3 rounded-md" @click.stop>
-                    <div class="flex flex-col p-6">
-                        <div class="flex justify-between items-center">
-                            <h2 class="text-3xl font-black leading-loose">チャンネルを作成する</h2> 
-                            <span class="text-4xl" @click="closeChannelModal">x</span>                    
-                        </div>
-                        <p>チャンネルはチームがコニュニケーションを取る場所です。特定のトピックに基づいてチャンネルを作ると良いでしょう。(例: #マーケティング)</p>
-                        <div class="mt-8 font-semibold">名前</div>
-                        <div class="my-3">
-                            <input 
-                                type="text" 
-                                class="w-full rounded border-gray-900 border-solid border p-3"
-                                v-model="channel"
-                            >
-                        </div>
-                        <div class="flex justify-end">
-                            <button 
-                            class="px-8 py-2 rounded bg-green-900 font-bold text-white"
-                            @click="addChannel"
-                            >作成</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <AddChannelModal :channelModal="parentChannelModal" @closeModal="closeChannelModal" />
             <PlusCircle @click.native="showChannelModal" />
         </div>
         <div
@@ -64,6 +36,7 @@
 <script>
 import Notification from '../components/icons/Notification';
 import PlusCircle from '../components/icons/PlusCircle';
+import AddChannelModal from '../views/AddChannelModal';
 import { mapGetters } from 'vuex';
 
 export default {
@@ -72,6 +45,7 @@ export default {
     components: {
         Notification,
         PlusCircle,
+        AddChannelModal
     },
 
     mounted() {
@@ -89,23 +63,7 @@ export default {
             this.$emit("updateChannelName", user);
         },
         showChannelModal() {
-            this.channelModal = true;
-        },
-        closeChannelModal() {
-            this.channelModal = false;
-        },
-        addChannel() {
-            let submitArray = {};
-            submitArray['channel_name'] = this.channel;
-            
-            axios.post('/api/channels', submitArray)
-                .then(response => {
-                    // this.channels.data.push(response.data);
-                    this.channelModal = false;
-                })
-                .catch(errors => {
-                    this.errors = errors.response.data.errors;
-                });
+            this.parentChannelModal = true;
         },
         getchannels() {
             axios.get('/api/channels')
@@ -115,6 +73,9 @@ export default {
                 .catch(errors => {
                     this.errors = errors.response.data.errors;
                 });
+        },
+        closeChannelModal() {
+            this.parentChannelModal = false;
         }
     },
 
@@ -135,9 +96,8 @@ export default {
                 },
             ],
             channels: [],
-            channelModal: false,
-            channel: '',
-            errors: ''
+            errors: '',
+            parentChannelModal: false,
         }
     },
 }
---
+++ resources/js/views/AddChannelModal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="z-10 fixed top-0 left-0 h-full w-full flex items-center justify-center" 
+        style="background-color:rgba(0, 0, 0, 0.5)"
+        v-show="channelModal"
+        @click="closeChannelModal"
+    >
+        <div class="z-20 bg-white text-gray-900 w-1/3 rounded-md" @click.stop>
+            <div class="flex flex-col p-6">
+                <div class="flex justify-between items-center">
+                    <h2 class="text-3xl font-black leading-loose">チャンネルを作成する</h2> 
+                    <span class="text-4xl" @click="closeChannelModal">x</span>                    
+                </div>
+                <p>チャンネルはチームがコニュニケーションを取る場所です。特定のトピックに基づいてチャンネルを作ると良いでしょう。(例: #マーケティング)</p>
+                <div class="mt-8 font-semibold">名前</div>
+                <div class="my-3">
+                    <input 
+                        type="text" 
+                        class="w-full rounded border-gray-900 border-solid border p-3"
+                        v-model="channel"
+                    >
+                </div>
+                <div class="flex justify-end">
+                    <button 
+                    class="px-8 py-2 rounded bg-green-900 font-bold text-white"
+                    @click="addChannel"
+                    >作成</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "AddChannelModal",
+
+    props: [
+        'channelModal',
+    ],
+
+    methods: {
+        closeChannelModal() {
+            this.$emit("closeModal");
+        },
+        addChannel() {
+            let submitArray = {};
+            submitArray['channel_name'] = this.channel;
+            
+            axios.post('/api/channels', submitArray)
+                .then(response => {
+                    // this.channels.data.push(response.data);
+                    this.channelModal = false;
+                })
+                .catch(errors => {
+                    this.errors = errors.response.data.errors;
+                });
+        },
+    },
+
+    data() {
+        return {
+            channel: '',
+        }
+    }
+
+}
+</script>
