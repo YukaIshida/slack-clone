41d908194b9bf9bbecff0c1f037fa0572e04ec78
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-28 01:53:25 +0900

    不要なボタン削除

--- resources/js/components/FileUpload.vue
+++ resources/js/components/FileUpload.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="content">
         <p><input type="file" v-on:change="fileSelected"></p>
-        <button @click="fileUpload">アップロード</button>
         <p v-show="showUserImage"><img :src="file_path"></p>
     </div>
 </template>
@@ -21,20 +20,6 @@ export default {
     methods:{
         fileSelected(event) {
             this.$emit('updateFileInfo', event.target.files[0]);
-        },
-        fileUpload() {
-            // ToDo Message.vueに移動
-            const formData = new FormData();
-            formData.append('file', this.fileInfo);
-
-            axios.post('/api/fileupload', formData)
-                .then(response => {
-                    this.showUserImage = true;
-                    this.file_path = response.data.file_path;
-                })
-                .catch(error => {
-                    console.log("Unable to upload file");
-                });
         }
     },
 };
