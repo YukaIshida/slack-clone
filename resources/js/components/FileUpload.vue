8c99832c585ee8df521577f25759701fd9200617
Yuka Ishida <stickey.yy@hotmail.co.jp>
2021-08-28 02:39:18 +0900

    ファイルアップロードをMessage.vue内に移動

--- resources/js/components/FileUpload.vue
+++ resources/js/components/FileUpload.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content">
+        <img v-show="uploadedImage" :src="uploadedImage" class="upload-image" />
         <p><input type="file" v-on:change="fileSelected"></p>
-        <p v-show="showUserImage"><img :src="file_path"></p>
     </div>
 </template>
 
@@ -13,18 +13,32 @@ export default {
     data() {
         return {
             showUserImage: false,
-            file_path: ''
+            file_path: '',
+            uploadedImage: '',
         }
     },
 
     methods:{
         fileSelected(event) {
-            this.$emit('updateFileInfo', event.target.files[0]);
-        }
+            let files = event.target.files[0];
+            this.$emit('updateFileInfo', files);
+            this.createImage(files);
+        },
+        // アップロードした画像を表示
+        createImage(file) {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                this.uploadedImage = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        },
     },
 };
 </script>
 
-<style>
-
+<style scoped>
+    .upload-image {
+        max-width: 100px;
+        max-height: 100px;
+    }
 </style>
--- resources/js/views/Message.vue
+++ resources/js/views/Message.vue
@@ -20,14 +20,15 @@
                         :placeholder="placeholder" 
                         v-model="message">
                     </textarea>
-
+                    <img v-show="uploadedImage" :src="uploadedImage" class="upload-image" />
+                    
                     <div class="bg-gray-100 p-2 flex justify-between" v-if="channel_id">
                         <button 
                             class="bg-green-900 text-sm text-white font-bold py-1 px-2 rounded"
                             @click="sendMessage"
                             >送信
                         </button>
-                        <FileUpload @updateFileInfo="getFileInfo($event)" />
+                        <p><input type="file" v-on:change="fileSelected"></p>
                     </div>
                 </div>
             </div>
@@ -73,7 +74,8 @@ export default {
             // ToDo messageとerrorsだけここに残して残りはVuexに移動
             message: '',
             errors: '',
-            fileInfo: ''
+            fileInfo: '',
+            uploadedImage: false
         }
     },
 
@@ -93,10 +95,27 @@ export default {
                 });
                 this.message = "";
         },
-        getFileInfo: function($event)  {
-            this.fileInfo = $event;
+        fileSelected($event)  {
+            let file = $event.target.files[0];
+            this.fileInfo = file;
+            this.createImage(file);
+        },
+        // アップロードした画像を表示
+        createImage(file) {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                this.uploadedImage = e.target.result;
+            };
+            reader.readAsDataURL(file);
         },
     },
 
 }
 </script>
+
+<style scoped>
+    .upload-image {
+        max-width: 100px;
+        max-height: 100px;
+    }
+</style>
